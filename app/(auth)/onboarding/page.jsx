a2a200671226666586fe8424968c31ed66aaaea9
Aleksey Rudyk <aleksey.rudyksm@gmail.com>
2024-02-12 12:56:53 +0200

    registration completed

--- app/(auth)/onboarding/page.jsx
+++ app/(auth)/onboarding/page.jsx
@@ -1,11 +1,14 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 async function Page() {
   const user = await currentUser();
-  const userInfo = {};
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
   const userData = {
     id: user?.id,
-    onjectId: userInfo?._id,
+    objectId: userInfo?._id,
     username: userInfo?.username || user?.username,
     name: userInfo?.name || user?.firstName || "",
     bio: userInfo?.bio || "",
